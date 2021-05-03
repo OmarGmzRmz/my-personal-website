e701f2b15ad53fff3827641616e8b2e53e3aa5b5
OmarGmzRmz <gomar8138@gmail.com>
2021-05-03 17:17:11 -0500

    Fixed send email function

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -12,7 +12,7 @@ import Typed, { TypedOptions } from 'typed.js';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-  //#region Swiper
+//#region Swiper
   index = 0;
   config: SwiperConfigInterface = {
     autoplay: {
@@ -30,14 +30,14 @@ export class LandingComponent implements OnInit {
   };
   //#endregion
 
-  //#region TypedJs
+//#region TypedJs
   typed: any;
   showElement1 = false;
   showElement2 = false;
   showElement3 = false;
   //#endregion
 
-//  #region  Particles
+//#region  Particles
   id = 'tsparticles';
   particlesOptions = {
     "interactivity": {
@@ -110,14 +110,14 @@ export class LandingComponent implements OnInit {
 contactForm = new FormGroup({
   toggleControl: new FormControl('whatsapp', [Validators.required]),
   nameControl: new FormControl('', [Validators.required]),
-  emailControl: new FormControl('', [Validators.email, (control: AbstractControl): {[key: string]: any} | null => {
+  emailControl: new FormControl('', [Validators.email, /* (control: AbstractControl): {[key: string]: any} | null => {
     const regularExpresion = /(?:[a-z0-9!#$%&'+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'+/=?^_`{|}~-]+)|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])")@(?:(?:[a-z0-9](?:[a-z0-9-][a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-][a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
     const match = regularExpresion.test(control.value);
     if (!match) {
       return { noMatchRegex: { errorMessage: 'Must enter a valid email address.' } };
     }
     return null;
-  }]),
+  } */]),
   messageControl: new FormControl('', [Validators.required, Validators.minLength(10)])
 });
 isSendEmail = false;
